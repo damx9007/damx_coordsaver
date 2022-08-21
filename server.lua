01c2db1b566cfef9bfa2db74b27d746f5e5fd493
Visloczki Adam <vadam9007@gmail.com>
2022-08-21 07:16:11 +0200

    coordSave- {} mentés hozzáadva

--- server.lua
+++ server.lua
@@ -5,7 +5,10 @@ AddEventHandler("damx_coordsaver:server:saveCoords", function(points)
     local saveTextTemplate = ''
 
         for k, v in pairs(points) do 
-            saveTextTemplate = saveTextTemplate .. ("vector3(%s, %s, %s) \n%s, %s, %s \n['x'] = %s, ['y'] = %s, ['z'] = %s \n"):format(
+            saveTextTemplate = saveTextTemplate .. ("vector3(%s, %s, %s) \n%s, %s, %s \n['x'] = %s, ['y'] = %s, ['z'] = %s \n{x=%s, y=%s, z=%s}"):format(
+                ESX.Math.Round(v.x, 2),
+                ESX.Math.Round(v.y, 2),
+                ESX.Math.Round(v.z, 2),
                 ESX.Math.Round(v.x, 2),
                 ESX.Math.Round(v.y, 2),
                 ESX.Math.Round(v.z, 2),
